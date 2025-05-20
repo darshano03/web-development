b6d1553d2e851e1220e6ec3dbcfabba423aaca81
darshano03 <odarshan03@gmail.com>
2025-05-20 12:38:41 +0530

    adding folder

---
+++ javascript 1/first.js
@@ -0,0 +1,34 @@
+/*console.log("hello world");
+let name="darshan";
+let age=22;
+x=null;
+y=undefined;
+ console.log(x);
+ let a;
+ console.log(a);
+ let isFollow=true;
+ console.log(isFollow);
+
+ const student = {
+        name:"darshan",
+        age:22,
+        cgpa:6.8,
+        isPass : true
+ };
+ 
+const product = {
+    name : "pen",
+    rating : 4,
+    offer : 5,
+    price :200
+};*/
+/*let items = [250,254,374,354,435,453];
+
+let idx =0;
+for(let val of items){
+    console.log('value at index ' + idx + ' = ' + val);
+    idx++;
+};*/
+
+let foodItems = ['rice','dal','curry','apple','banana'];
+console.log(foodItems);
